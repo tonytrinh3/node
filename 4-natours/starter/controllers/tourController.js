01ad6354c0f541aa83bf7f5b0a6f84ef4f14727d
Tony Trinh <tony@bokksu.com>
2021-09-08 21:37:33 -0400

    removed all files that are in the gitignore

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -37,29 +37,32 @@ exports.getAllTours = async (req, res) => {
       requestedAt: req.requestTime,
       results: tours.length,
       data: {
-        tours
-      }
-  
+        tours,
+      },
     });
-  } catch (err){
+  } catch (err) {
     res.status(404).json({
-      status: "fail",
-      message: err
-    })
+      status: 'fail',
+      message: err,
+    });
   }
- 
 };
 
-exports.getTour = (req, res) => {
-  const id = req.params.id * 1; //this is javasript trick to convert string to int
-  // const tour = tours.find((el) => el.id === id);
-
-  // res.status(200).json({
-  //   status: 'success',
-  //   data: {
-  //     tour,
-  //   },
-  // });
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id)
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
 exports.createTour = async (req, res) => {
