fe1c169bc4ea41947f0da8b69daed393ca7bb7da
Tony Trinh <tony@bokksu.com>
2021-09-13 21:47:33 -0400

    made a class

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -1,6 +1,13 @@
 const fs = require('fs');
 const Tour = require('./../models/tourModel');
 
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
+
 // const tours = JSON.parse(
 //   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 // );
@@ -26,22 +33,73 @@ exports.checkBody = (req, res, next) => {
   next();
 };
 
-exports.getAllTours = async (req, res) => {
-  //console.log(req.requestTime);
- 
-  try {
-    const queryObj = {...req.query};
-    const excludedFields = ['page','sort','limit','fields'];
-    excludedFields.forEach(el => delete queryObj[el])
-    console.log(req.query, queryObj)
+class APIFeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+
+  filter() {
+    const queryObj = { ...this.queryString };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+    //console.log('req.query', req.query, queryObj);
 
     //take query string and allow less than, greater than etc to be used in query
     let queryStr = JSON.stringify(queryObj);
-    queryStr =   queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-    console.log(JSON.parse(queryStr))
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    //console.log(JSON.parse(queryStr));
 
-    const tours = await Tour.find(JSON.parse(queryStr))  ;
-    
+    this.query = this.query.find(JSON.parse(queryStr));
+
+    return this;
+  }
+
+  sort() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(',').join(' ');
+      //console.log(sortBy)
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort('-createdAt');
+    }
+
+    return this; //this returned is required to chain methods. you are returning the entire object
+  }
+
+  limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(',').join(' ');
+      this.query = this.query.select(fields);
+    } else {
+      this.query = this.query.select('-__v');
+    }
+
+    return this;
+  }
+
+  paginate() {
+    const page = this.queryString.page * 1 || 1;
+    // console.log(`req.query.page`,req.query.page)
+    const limit = this.queryString.limit * 1 || 100;
+    // console.log(`req.query.limit`,req.query.limit)
+    const skip = (page - 1) * limit;
+    this.query = this.query.skip(skip).limit(limit);
+    return this;
+  }
+}
+
+exports.getAllTours = async (req, res) => {
+  //console.log(req.requestTime);
+
+  try {
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+
+    const tours = await features.query;
 
     res.status(200).json({
       status: 'success',
@@ -61,7 +119,7 @@ exports.getAllTours = async (req, res) => {
 
 exports.getTour = async (req, res) => {
   try {
-    const tour = await Tour.findById(req.params.id)
+    const tour = await Tour.findById(req.params.id);
     res.status(200).json({
       status: 'success',
       data: {
@@ -95,41 +153,38 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.updateTour =async  (req, res) => {
-  try{
-    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body,{
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
-      runValidators: true
-    })
+      runValidators: true,
+    });
 
     res.status(200).json({
       status: 'success',
       data: {
-        tour
+        tour,
       },
     });
-  } catch(err){
+  } catch (err) {
     res.status(400).json({
       status: 'fail',
       message: err,
     });
   }
-  
 };
 
 exports.deleteTour = async (req, res) => {
-
-  try{
+  try {
     await Tour.findByIdAndDelete(req.params.id);
     res.status(204).json({
       status: 'success',
       data: null,
     });
-  } catch (err){
+  } catch (err) {
     res.status(400).json({
       status: 'fail',
       message: err,
     });
   }
- 
 };
